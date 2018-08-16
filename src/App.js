a6b0616205626a2293350e3e322d74fc97f06136
Basavannevva <Basavannevva@xelpmoc.in>
2018-08-16 15:20:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Courses from './Courses';
+const client = new ApolloClient({
+  uri: "https://vm8mjvrnv3.lp.gql.zone/graphql"
+});
+const App = () => (
+  <ApolloProvider client={client}>
+    <div className="container">
+      <nav className="navbar navbar-dark bg-primary">
+        <a className="navbar-brand" href="#">React and GraphQL - Sample Application</a>
+      </nav>
+      <div>
+        <Courses />
+      </div>
+    </div>
+  </ApolloProvider>
+);
+export default App;
